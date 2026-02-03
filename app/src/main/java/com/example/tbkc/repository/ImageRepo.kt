269b074b7a5b4a722911554b5cd74923c44f2eb3
anilkc01 <mynameisanilkc@icloud.com>
2026-02-03 16:27:09 +0545

    image upload too cloudinary

--- app/src/main/java/com/example/tbkc/repository/ImageRepo.kt
+++ app/src/main/java/com/example/tbkc/repository/ImageRepo.kt
@@ -1,4 +1,10 @@
 package com.example.tbkc.repository
 
+import android.content.Context
+import android.net.Uri
+
 interface ImageRepo {
+    fun  uploadImage(context: Context, imageUri: Uri?, callback: (Boolean,String?)-> Unit)
+
+    fun getFileNameFromUri(context: Context,imageUri: Uri?):String?
 }
--- app/src/main/java/com/example/tbkc/viewModel/ImageViewModel.kt
+++ app/src/main/java/com/example/tbkc/viewModel/ImageViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.tbkc.viewModel
 
-class ImageViewModel {
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import com.example.tbkc.repository.ImageRepo
+
+class ImageViewModel(val repo: ImageRepo) : ViewModel()  {
+    fun  uploadImage(context: Context, imageUri: Uri, callback: (Boolean, String?) -> Unit){
+        repo.uploadImage(context,imageUri,callback)
+    }
 }
