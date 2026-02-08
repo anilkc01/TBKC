1d4b38a56d75b894f4810a3e97003cab40d2058e
anilkc01 <mynameisanilkc@icloud.com>
2026-02-08 20:35:41 +0545

    add trek page

--- app/src/main/java/com/example/tbkc/view/AddTrek.kt
+++ app/src/main/java/com/example/tbkc/view/AddTrek.kt
@@ -1,2 +1,27 @@
 package com.example.tbkc.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun AddTrek() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Add Trek Screen",
+            color = Color.White,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+
+}
